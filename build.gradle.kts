16c40e558e1e53fc2a60b29a650a4ca87521fea6
devinfritz <devinfritz@gmx.de>
2024-10-14 10:00:19 +0200

    Fix publish, removed gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,20 @@
-val kotlin_version: String by project
-val logback_version: String by project
-val ktor_version: String by project
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost
+
+val kotlin_version: String = "2.0.21"
+val ktor_version: String = "3.0.0"
 
 plugins {
     kotlin("jvm") version "2.0.21"
     id("io.ktor.plugin") version "3.0.0"
-    `maven-publish`
-    id("signing")
+    id("com.vanniktech.maven.publish") version "0.28.0"
+    id("org.jetbrains.dokka") version "1.9.20"
 }
 
 group = "statix.org"
-version = "0.0.1"
+version = "1.0.0"
+val projectVersion: String = version.toString()
 
 repositories {
     mavenCentral()
@@ -22,12 +26,66 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
 
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "io.github.staticfx"
-            artifactId = "ktor-middleware"
-            version = "1.0.0"
+}
+
+mavenPublishing {
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaHtml"),
+            sourcesJar = true,
+        ),
+    )
+
+    coordinates(
+        groupId = "io.github.staticfx",
+        artifactId = "ktor-middleware",
+        version = projectVersion,
+    )
+
+    pom {
+        name.set("KTOR Middleware")
+        description.set("Middlewares for KTOR")
+        inceptionYear.set("2024")
+        url.set("https://github.com/StaticFX/ktor-middleware")
+
+        developers {
+            developer {
+                name.set("Devin Fritz")
+                email.set("devinfritz@gmx.de")
+            }
         }
+
+        licenses {
+            license {
+                name.set("gpl-3.0")
+                url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/StaticFX/ktor-middleware")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks {
+    shadowJar {
+        enabled = false
     }
 }
